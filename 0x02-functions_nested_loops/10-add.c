39fb184f8acdfa3fa445800ecef6c9d63e71568a
Jerry-kjk <jerrykajik@gmail.com>
2023-03-21 17:39:58 -0700

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - a function that adds two integers and returns the result.
+ *
+ * @x: parameter 1
+ *
+ * @y: parameter 2
+ *
+ * Return: x+y
+ *
+ */
+
+int add(int x, int y)
+{
+	return (x + y);
+}
