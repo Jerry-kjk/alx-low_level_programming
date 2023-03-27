4ee530c92cfb9cfc3042e326ba358e932c0c0f97
Jerry-kjk <jerrykajik@gmail.com>
2023-03-27 14:49:36 -0700

    Half the lies they tell about me aren't true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * puts2 - Prints one char out of two of a string.
+ * @str: The string containing characters.
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0 ; str[i] != '\0' ; i++)
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
