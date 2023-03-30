969fea9271b240eada8fff417b4a8883548a6d13
Jerry-kjk <jerrykajik@gmail.com>
2023-03-30 06:59:02 -0700

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -15,14 +15,7 @@ int _strcmp(char *s1, char *s2)
 	for (i = 0 ; s1[i] && s2[i]; i++)
 	{
 		if (s1[i] != s2[i])
-		{
-			if (s1[i] < s2[i])
-				return (s1[i] - s2[i]);
-			else if (s1[i] > s2[i])
-				return (s1[i] - s2[i]);
-		}
-		else
-			return (0);
+			return (s1[i] - s2[i]);
 	}
-	return (0);
+	return (s1[i] - s2[i]);
 }
