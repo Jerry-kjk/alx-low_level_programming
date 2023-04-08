1a3e5bb4586ba764df38cb27c897b81bb6e5e93d
Jerry-kjk <jerrykajik@gmail.com>
2023-04-08 08:25:22 -0700

    Neither irony nor sarcasm is argument

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,44 +1,6 @@
 #include "main.h"
 #include <stdio.h>
-
-/**
- * _atoi - converts a string to an integer.
- * @s: string to be converted.
- * Return: int converted.
- */
-
-int _atoi(char *s)
-{
-	int a = 0;
-	int b = 0;
-	int c = 0;
-	int len = 0;
-	int x = 0;
-	int digit = 0;
-
-	while (s[len] != '\0')
-		len++;
-	while (a < len && x == 0)
-	{
-		if (s[a] == '-')
-			++b;
-		if (s[a] >= '0' && s[a] <= 9)
-		{
-			digit = s[a] - '0';
-			if (b % 2)
-				digit = -digit;
-			c = c * 10 + digit;
-			x = 1;
-			if (s[a + 1] < '0' || s[a + 1] > '9')
-				break;
-			x = 0;
-		}
-		a++;
-	}
-	if (x == 0)
-		return (0);
-	return (c);
-}
+#include <stdlib.h>
 
 /**
  * main - a program that multiplies two numbers.
@@ -50,20 +12,21 @@ int _atoi(char *s)
 
 int main(int argc, char *argv[])
 {
-	int prod;
-	int num1;
-	int num2;
+	int i;
+	int j;
 
-	if (argc < 3 || argc > 3)
+	j = 1;
+
+	if (argc == 1 || argc == 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-
-	num1 = _atoi(argv[1]);
-	num2 = _atoi(argv[2]);
-	prod = num1 * num2;
-
-	printf("%d\n", prod);
+	else
+	{
+		for (i = 1; i < 3; i++)
+			j *= atoi(argv[i]);
+		printf("%d\n", j);
+	}
 	return (0);
 }
