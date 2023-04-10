ed71712025f9f0f6a68de2fbc06a23647a594608
Jerry-kjk <jerrykajik@gmail.com>
2023-04-10 02:22:24 -0700

    To infinity and beyond

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -13,21 +13,21 @@
 
 int main(int argc, char **argv)
 {
-	    int sum = 0;
-	        int i;
+	int sum = 0;
+	int i;
 
-		    for (i = 1; i < argc; i++)
-			        {
-					        if (is_number(argv[i]))
-							            sum += atoi(argv[i]);
-						        else
-								        {
-										            printf("Error\n");
-											                return (1);
-													        }
-							    }
-		        printf("%d\n", sum);
-			    return (0);
+	for (i = 1; i < argc; i++)
+	{
+		if (is_number(argv[i]))
+			sum += atoi(argv[i]);
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
 }
 
 /**
@@ -38,13 +38,13 @@ int main(int argc, char **argv)
 
 int is_number(char *s)
 {
-	    if (*s == '\0')
-		            return (0);
-	        while (*s != '\0')
-			    {
-				            if (*s < '0' || *s > '9')
-						                return (0);
-					            s++;
-						        }
-		    return (1);
+	if (*s == '\0')
+		return (0);
+	while (*s != '\0')
+	{
+		if (*s < '0' || *s > '9')
+			return (0);
+			s++;
+	}
+	return (1);
 }
