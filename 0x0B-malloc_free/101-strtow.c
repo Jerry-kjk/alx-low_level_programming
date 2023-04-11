8ce7660a0060b3077a7053378e3b3dfcf581bac6
Jerry-kjk <jerrykajik@gmail.com>
2023-04-11 05:57:28 -0700

    I will show you how great I am

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,75 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * count_words - Count the number of words in a string.
+ * @str: The string to count words in.
+ * Return: The number of words in the string.
+ */
+
+int count_words(char *str);
+
+int count_words(char *str)
+{
+	int count = 0;
+	int in_word = 0;
+
+	while (*str != '\0')
+	{
+		if (*str == ' ')
+			in_word = 0;
+		else if (in_word == 0)
+		{
+			in_word = 1;
+			count++;
+		}
+		str++;
+	}
+	return (count);
+}
+
+/**
+ * strtow - a function that splits a string into words.
+ * @str: The string to split.
+ * Return: NULL if str == NULL or str == "".
+ */
+
+char **strtow(char *str)
+{
+	int i;
+	int j;
+	int k;
+	int len = 0;
+	int words;
+	char **arr;
+
+	if (str == NULL || *str == '\0')
+		return (NULL);
+	words = count_words(str);
+	if (words == 0)
+		return (NULL);
+	arr = malloc((words + 1) * sizeof(char *));
+	if (arr == NULL)
+		return (NULL);
+	for (i = 0, j = 0; i < words; ++i)
+	{
+		while (str[j] == ' ')
+			j++;
+		while (str[j + len] != ' ' && str[j + len] != '\0')
+			len++;
+		arr[i] = malloc((len + 1) * sizeof(char));
+		if (arr[i] == NULL)
+		{
+			for (k = 0; k < i; ++k)
+				free(arr[k]);
+			free(arr);
+			return (NULL);
+		}
+		for (k = 0; k < len; ++k)
+			arr[i][k] = str[j + k];
+		arr[i][len] = '\0';
+		j += len;
+	}
+	arr[i] = NULL;
+	return (arr);
+}
