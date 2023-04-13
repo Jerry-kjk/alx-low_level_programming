d2260d6b2f5749bf63e789fac1bf8333c1990d7d
Jerry-kjk <jerrykajik@gmail.com>
2023-04-13 06:12:31 -0700

    string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,26 +10,29 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int l1, l2, i;
+	unsigned int l1 = 0, l2 = 0, i, j;
 	char *str;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	
-	l1 = strlen(s1);
-	l2 = strlen(s2);
-
+	while (s1[l1])
+		l1++;
+	while (s2[l2])
+		l2++;
 	if (n >= l2)
 		n = l2;
-	str = malloc(sizeof(char) * (l1 + n + 1));
+	str = (char *) malloc(sizeof(char) * (l1 + n + 1));
 	if (str == NULL)
 		return (NULL);
-	for (i = 0; i < l1; i++)
+	for (i = 0; s1[i] != '\0'; i++)
 		str[i] = s1[i];
-	for (i = 0; i < n; i++)
-		str[l1 + 1] = s2[i];
-	str[l1 + n] = '\0';
+	for (j = 0; j < n && s2[j] != '\0'; j++)
+	{
+		str[i] = s2[j];
+		i++;
+	}
+	str[i] = '\0';
 	return (str);
 }
