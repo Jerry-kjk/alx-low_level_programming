335306690b1595fead6982f8849945cb4f2302f8
Jerry-kjk <jerrykajik@gmail.com>
2023-04-13 10:52:18 -0700

    _realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -20,7 +20,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (NULL);
 	}
 	if (ptr == NULL)
-		return (malloc(new_size));
+		return malloc(new_size);
 	if (new_size == old_size)
 		return (ptr);
 	new_ptr = malloc(new_size);
