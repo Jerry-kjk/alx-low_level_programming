f66b80254d4d8abc233c834c45817d72324910a1
Jerry-kjk <jerrykajik@gmail.com>
2023-04-29 07:17:42 -0700

    Free

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -13,11 +13,11 @@ void free_listint2(listint_t **head)
 	{
 		return;
 	}
-	while (*head != NULL)
+	while (*head)
 	{
-		tmp = *head;
-		*head = (*head)->next;
-		free(tmp);
+		tmp = (*head)->next;
+		*head = tmp;
+		free(*head);
 	}
 	*head = NULL;
 }
