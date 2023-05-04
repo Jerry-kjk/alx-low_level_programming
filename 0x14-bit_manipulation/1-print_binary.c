764d44099d6863cd052d9ce3c5ec68e148e24ecd
Jerry-kjk <jerrykajik@gmail.com>
2023-05-04 03:33:13 -0700

    1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,20 +7,7 @@
 
 void print_binary(unsigned long int n)
 {
-	unsigned int i;
-	unsigned long int temp = 1;
-
-	temp = temp << ((sizeof(unsigned long int) * 8) - 1);
-	for (i = 0; i < sizeof(unsigned long int) * 8; i++)
-	{
-		if ((n & temp) == 0)
-		{
-			_putchar('0');
-		}
-		else
-		{
-			_putchar('1');
-		}
-		temp = temp >> 1;
-	}
+	if (n > 1)
+		print_binary(n >> 1);
+	_putchar((n & 1) + '0');
 }
