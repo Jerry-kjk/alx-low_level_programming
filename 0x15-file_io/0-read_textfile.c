4e3a6f19f34018ebb9b5e62f8b71402102c2841d
Jerry-kjk <jerrykajik@gmail.com>
2023-05-08 14:36:42 -0700

    Tread lightly, she is near

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -27,23 +27,10 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 	{
-		free(buffer);
 		return (0);
 	}
 	r = read(fd, buffer, letters);
-	if (r == -1)
-	{
-		free(buffer);
-		close(fd);
-		return (0);
-	}
 	w = write(STDOUT_FILENO, buffer, r);
-	if (w == -1 || w != (ssize_t)letters)
-	{
-		free(buffer);
-		close(fd);
-		return (0);
-	}
 	free(buffer);
 	close(fd);
 	return (w);
